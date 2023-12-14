9a4f985e582bbdd247fa96bbb07bed43fef7dd65
BHObserver <BHObserver0@gmail.com>
2023-12-15 01:39:30 +0600

    Update 20231214184439_create_comments.rb

--- db/migrate/20231214184439_create_comments.rb
+++ db/migrate/20231214184439_create_comments.rb
@@ -1,11 +1,14 @@
-class CreateComments < ActiveRecord::Migration[7.1]
+class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
       t.text :text
       t.datetime :updated_at
       t.datetime :created_at
+
+      t.index :user_id
+      t.index :post_id
 
       t.timestamps
     end
