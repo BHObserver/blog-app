2102d1811fcbcea8582fc9443a848c74f9778e95
BHObserver <BHObserver0@gmail.com>
2023-12-15 01:53:11 +0600

    Update 20231214185124_create_likes.rb

--- db/migrate/20231214185124_create_likes.rb
+++ db/migrate/20231214185124_create_likes.rb
@@ -1,13 +1,8 @@
-class CreateLikes < ActiveRecord::Migration[6.1]
+class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
-      t.datetime :created_at
-      t.datetime :updated_at
-
-      t.index :user_id
-      t.index :post_id
 
       t.timestamps
     end
