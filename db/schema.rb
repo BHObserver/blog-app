eb8b5293498fd032995a627bd8b3da803be3feff
BHObserver <BHObserver0@gmail.com>
2023-12-15 03:55:53 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_14_185124) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "author_id"
+    t.bigint "user_id"
     t.bigint "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -48,13 +48,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_14_185124) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter", default: 0
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
