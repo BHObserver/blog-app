53149f8524bbba2f06e61d3ef22a5b6e3ea0ec4d
BHObserver <BHObserver0@gmail.com>
2023-12-15 23:24:32 +0600

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
+# app/models/like.rb
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # Custom method: Updates the likes counter for a post
+  def update_post_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
