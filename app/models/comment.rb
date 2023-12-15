e43ff6c2bc3a3810e9229500ffa78a426e72d0db
BHObserver <BHObserver0@gmail.com>
2023-12-15 18:25:19 +0600

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # Custom method: Updates the comments counter for a post
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
+
 end
