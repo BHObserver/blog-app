26917bffd7f4fdbdf25f4281d9890101f4c7dccd
BHObserver <BHObserver0@gmail.com>
2023-12-16 19:11:48 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,6 @@ class User < ApplicationRecord
     update(likes_counter: likes.count)
   end
 
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
