23aa8673455850aff341dea05aba5b55e5091147
BHObserver <BHObserver0@gmail.com>
2023-12-20 14:20:44 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   # root 'pages#hello'
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-
-    end
+  resources :posts, only: [:index, :show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
