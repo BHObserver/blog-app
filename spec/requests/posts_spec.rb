8c2313599cc21875ff2336a47cf50fd24a2d3168
BHObserver <BHObserver0@gmail.com>
2023-12-21 23:03:52 +0600

    Update posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET #show' do
-  it 'includes correct placeholder text in the response body' do
+  describe 'GET #index'
+  it 'return a success response' do
     user = FactoryBot.create(:user)
-    post = FactoryBot.create(:post, author: user)
-    get user_post_path(user, post)
-    expect(response.body).to include('Great Quotes')
+    FactoryBot.create(:post, author: user)
+    get user_posts_path(user)
+    expect(response).to have_http_status(:success)
   end
 
+  it 'renders the index template' do
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:post, author: user)
+    get user_posts_path(user)
+    expect(response).to render_template(:index)
+  end
+
+  it 'includes correct placeholder text in the response body' do
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:post, author: user)
+    get user_posts_path(user)
+    expect(response.body).to include('Great Quotes')
+  end
+end
+
+describe 'GET #show' do
   it 'returns a success responese' do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post, author: user)
@@ -23,28 +39,10 @@ RSpec.describe 'Posts', type: :request do
     expect(response).to render_template(:show)
   end
 
-end
-
-  describe 'GET #index'
-    it 'renders the index template' do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:post, author: user)
-      get user_posts_path(user)
-      expect(response).to render_template(:index)
-    end
-
-    it 'includes correct placeholder text in the response body' do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:post, author: user)
-      get user_posts_path(user)
-      expect(response.body).to include('Great Quotes')
-    end
-
-    it 'return a success response' do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:post, author: user)
-      get user_posts_path(user)
-      expect(response).to have_http_status(:success)
-    end
+  it 'includes correct placeholder text in the response body' do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post, author: user)
+    get user_post_path(user, post)
+    expect(response.body).to include('Great Quotes')
   end
 end
