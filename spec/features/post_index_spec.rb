6a1072b37c5b1eac9634f041d91b66defe0978cf
Surafels <ssurafel4@gmail.com>
2024-01-12 20:33:44 +0300

    Add test to post index spec

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -11,3 +11,14 @@ RSpec.describe 'Post', type: :feature do
     @comment2 = Comment.create!(id: 2, post_id: @post2.id, user_id: @user1.id, text: 'Hello Ron!')
     @like1 = Like.create!(id: 1, post_id: @post1.id, user_id: @user1.id)
   end
+
+  describe 'Index page' do
+    it "should display the user's profile picture" do
+      visit user_posts_path(@user1)
+      expect(page).to have_css("img[src*='https://unsplash.com/photos/1.jpg']")
+    end
+    it "should display the user's username" do
+      visit user_posts_path(@user1)
+      expect(page).to have_content(@user1.name)
+    end
+  end
