03962139612affa90d4532c436c1678d567b6364
BHObserver <BHObserver0@gmail.com>
2024-01-14 00:40:19 +0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+
 
   def index
     @users = User.includes(:posts).all
@@ -8,8 +8,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if @user.nil?
-      flash[:alert] = 'User not found.'
+      flash[:notice] = 'Signed out successfully.'
       redirect_to root_path
     end
+  end
+
+  def user_sign_out
+    sign_out current_user
+    redirect_to new_user_session_path notice: 'Signed out successfully.'
   end
 end
