2745b24debc59663017ca3c2b9700232df7dbdc2
BHObserver <BHObserver0@gmail.com>
2024-01-13 11:36:11 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  # get 'pages/hello'
-  # root 'pages#hello'
-  root 'users#index'
+
+  devise_for :users
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+  get '/user_sign_out', to: 'users#user_sign_out'
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:new, :create]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
