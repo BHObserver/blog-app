a349f89ea5998fa850d6bc989261185014558d71
BHObserver <BHObserver0@gmail.com>
2024-01-15 09:28:59 +0600

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,21 @@
 class CommentsController < ApplicationController
-  before_action :set_user_and_post, only: %i[new create]
+  load_and_authorize_resource
 
   def new
-    store_referer
-
+    @user = current_user
+    @post = Post.find_by(id: params[:id])
     @comment = Comment.new
   end
 
   def create
-    @comment = @post.comments.build(comment_params.merge(user_id: @user.id))
-
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @post = Post.find_by(id: params[:post_id])
+    @comment.post = @post
     if @comment.save
-      flash[:success] = 'Comment saved successfully'
-      redirect_back_or_default(user_post_comments_path(@user, @post))
+      @post.save
+      redirect_to user_post_path(@comment.user, @post)
     else
-      flash.now[:error] = 'Error: Comment could not be saved'
       render :new
     end
   end
@@ -31,19 +32,5 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text)
-  end
-
-  def set_user_and_post
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:post_id])
-  end
-
-  def store_referer
-    session[:referer] = request.referer
-  end
-
-  def redirect_back_or_default(default)
-    redirect_to(session[:referer] || default)
-    session.delete(:referer)
   end
 end
