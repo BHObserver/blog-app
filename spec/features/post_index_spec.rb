484c96cfc0034d75ce130b80097d30bba5cfbf20
BHObserver <BHObserver0@gmail.com>
2024-01-17 04:04:58 +0600

    Update post_index_spec.rb

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Post', type: :feature do
                           posts_counter: 1)
     @post1 = Post.create!(id: 1, author_id: @user1.id, title: 'First Post', text: 'First text', comments_counter: 2,
                           likes_counter: 1)
-    @post2 = Post.create!(id: 2, author_id: @user1.id, title: 'First Post', text: 'First text', comments_counter: 2,
+    @post2 = Post.create!(id: 2, author_id: @user1.id, title: 'Second Post', text: 'Second text', comments_counter: 2,
                           likes_counter: 1)
     @comment1 = Comment.create!(id: 1, user_id: @user1.id, post_id: @post1.id, text: 'Hello David!')
     @comment2 = Comment.create!(id: 2, post_id: @post2.id, user_id: @user1.id, text: 'Hello Ron!')
@@ -34,6 +34,10 @@ RSpec.describe 'Post', type: :feature do
       visit user_posts_path(@user1)
       expect(page).to have_content(@post1.text)
     end
+    it 'should display posts count' do
+      visit user_posts_path(@user1)
+      expect(page).to have_content(@user1.posts.count)
+    end
     it 'should display the first comments on a post' do
       visit user_posts_path(@user1)
       expect(page).to have_content(@comment1.text)
@@ -50,6 +54,11 @@ RSpec.describe 'Post', type: :feature do
     it 'Should display the section to create a new post' do
       visit user_posts_path(@user1)
       expect(page).to have_content('Create New Post')
+    end
+    it 'Should redirects to post show page when clicked' do
+      visit user_posts_path(@user1)
+      click_link @post1.title
+      expect(current_path).to eq(user_post_path(@user1, @post1))
     end
   end
 end
