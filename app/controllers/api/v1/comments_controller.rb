dedae0f590bc89d37f57224e12d2d0dc270b5b46
BHObserver <BHObserver0@gmail.com>
2024-01-18 12:45:16 +0600

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,25 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post
 
   def index
-    post = Post.find(params[:post_id])
-    comments = post.comments
-    render json: { comments: }
+    comments = @post.comments
+    render json: { comments: comments }
   end
 
   def create
-    post = Post.find(params[:post_id])
-    comment = post.comments.new(comment_params)
+    comment = @post.comments.new(comment_params)
     comment.user = current_user
 
     if comment.save
-      render json: { comment: }, status: :created
+      render json: { comment: comment }, status: :created
     else
       render json: { error: comment.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
